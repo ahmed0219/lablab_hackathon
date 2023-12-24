ef5a93cc4b8a18e6261ac25977887548048c2aec
ahmed0219 <101904462+ahmed0219@users.noreply.github.com>
2023-12-24 14:04:23 +0100

    Update user_interface.py

--- code/user_interface.py
+++ code/user_interface.py
@@ -7,14 +7,14 @@ def chat_app():
 
     col1, col2, col3 = st.columns(3)
 
-    # Apply centered-container style to the whole app
+   
     st.markdown("<div style='display: flex; align-items: center; justify-content: center;'>", unsafe_allow_html=True)
 
     with col2:
         image_path = "../ASSETS/new.png"
         st.image(image_path, caption='')
 
-    # Text below the image
+    
     st.markdown("<p style='text-align: center; font-size:20px;'>How can I help you today?</p>", unsafe_allow_html=True)
 
     if 'messages' not in st.session_state:
@@ -26,14 +26,14 @@ def chat_app():
 
   
     if prompt := st.chat_input(""):
-        # Add user message to chat history
+        
 
         # Display user message in chat message container
         with st.chat_message("user",avatar="../ASSETS/a.png"):
             st.markdown(prompt)
         st.session_state.messages.append({"role": "user", "content": prompt,"avatar":"../ASSETS/a.png"})
 
-        # Display assistant response in chat message container
+        
         with st.chat_message("assistant", avatar="../ASSETS/log.png"):
             message_placeholder=st.empty()
             response = generate_response(prompt)
@@ -41,10 +41,10 @@ def chat_app():
             st.session_state.messages.append({"role": "assistant", "content": response['response'],"avatar":"../ASSETS/log.png"})
             print(response['show'])
 
-            # Scroll to the bottom
+            
             st.markdown("<script>document.getElementById('root').scrollIntoView({behavior: 'smooth', block: 'end'});</script>", unsafe_allow_html=True)
 
-    # Close the centered-container div
+   
     st.markdown("</div>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
