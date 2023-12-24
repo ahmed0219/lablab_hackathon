4166d4c24629b9892c801ddbbcfb6c8ee4dc5593
ahmed0219 <101904462+ahmed0219@users.noreply.github.com>
2023-12-24 14:03:23 +0100

    Update fn.py

--- code/fn.py
+++ code/fn.py
@@ -6,17 +6,17 @@ def readjson():
         data = json.load(json_file)
     return data    
 def generate_response(prompt):
-    # Read the JSON file
+   
    
     data=readjson()
-    # Assume the JSON contains a list of items, modify accordingly based on your JSON structure
+   
     for item in data:
         if "Questions" in item and item["Questions"].lower() == prompt.lower():
             print(item.get("Answers"))
-            # Return the corresponding answer if found
+            
             return {"response":item.get("Answers"),"show":True}
 
-    # If the question is not found
+   
     return {"response":"as ai model i can't answer that ","show":False}
 
 
